7b77461a86c8e6606bfa451ed08fe1a9d2c6a0d7
Julian Miller <73328279+docjulz@users.noreply.github.com>
2022-04-06 19:43:46 -0700

    updated useState Hooks

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import './App.css';
 // Import Components
 import Form from './components/form';
+import TodoList from './components/todoList';
+// Import Hooks
+import { useState } from "react";
 
 function App() {
+  // Add the State here
+  const [inputText, setInputText] = useState("");
   return (
     <div className="App">
         <header>
           <h1>SouthCounty<span>.</span>Creative Todo List</h1>
         </header>
-        <Form/>
+        <Form setInputText={setInputText} />
+        
+        {/* Add setInputText to copy from form input to items in list */}
+        <TodoList setInputText={setInputText} />
     </div>
   );
 }
